b6433df41dd389236f8e442813d6a1eb39a7baa4
future_zjj <1805862661@qq.com>
2024-09-21 16:26:28 +0800

    新增web

--- main.go
+++ main.go
@@ -2,11 +2,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"time"
 )
+
+// 定义一个简单的响应结构体
+type Response struct {
+	Message string `json:"message"`
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Welcome to the Go Web API!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Hello, World!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+// 处理 "/goodbye" 路径的请求
+func handleGoodbye(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Goodbye!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func welcome(w http.ResponseWriter, r *http.Request) {
+
+}
 
 type Config struct {
 	Port int    `yaml:"port"`
@@ -14,6 +45,11 @@ type Config struct {
 }
 
 func main() {
+
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/hello", handleHello)
+	http.HandleFunc("/goodbye", handleGoodbye)
+
 	config := Config{}
 
 	// 读取 YAML 配置文件
@@ -29,4 +65,6 @@ func main() {
 	}
 
 	fmt.Printf("Host: %s, Port: %d\n", config.Host, config.Port)
+
+	time.Sleep(100000)
 }
